7486e0aaa3758a66356515acacc0b4947636d8ce
crazzmc <crazzmc@gmail.com>
2023-03-28 23:24:37 -0400

    move files automatically

---
+++ src/main/java/MoveFiles.java
@@ -0,0 +1,26 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.StandardCopyOption;
+import java.util.Collection;
+public class MoveFiles {
+
+    static String[] FILE_NAMES = {"http-api", "polish", "rlawt", "runelite-api", "runelite-jshell", "runescape-api"};
+    public static void main(String[] args) throws IOException {
+        String[] extensions = {"jar"};
+        Collection<File> files = FileUtils.listFiles(new File(System.getProperty("user.dir") + "/unstable/"), extensions, true);
+        files.forEach(file -> {
+            for (String fileName : FILE_NAMES) {
+                if (file.getName().contains(fileName)) {
+                    try {
+                        FileUtils.copyFile(file, new File(System.getProperty("user.dir") + "/stable/" + file.getName()), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+    }
+
+}
