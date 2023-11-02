64794ea11e001d8a3ee4e981b260a799fde19521
crazzmc <crazzmc@gmail.com>
2023-11-01 21:45:55 -0400

    update snapshot

--- src/main/java/MoveFiles.java
+++ src/main/java/MoveFiles.java
@@ -17,12 +17,13 @@ public class MoveFiles {
      * push hosting files
      */
     static String[] FILE_NAMES = {"http-api", "polish", "rlawt", "runelite-api", "runelite-jshell", "runescape-api", "runelite-client"};
+    static String SNAPSHOT = "4.2.5";
     public static void main(String[] args) throws IOException {
         String[] extensions = {"jar"};
         Collection<File> files = FileUtils.listFiles(new File(System.getProperty("user.dir") + "/unstable/"), extensions, true);
         files.forEach(file -> {
             for (String fileName : FILE_NAMES) {
-                if (file.getName().contains(fileName)) {
+                if (file.getName().contains(fileName) && file.getName().contains(SNAPSHOT)) {
                     try {
                         FileUtils.copyFile(file, new File(System.getProperty("user.dir") + "/stable/" + file.getName()), StandardCopyOption.REPLACE_EXISTING);
                     } catch (IOException e) {
